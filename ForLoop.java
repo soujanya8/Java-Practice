69715e98b7a9a8a3631eab2bfe3d16b37f9c878f
soujanya8 <soujanyagodugu8@gmail.com>
2025-03-04 22:20:13 -0700

    newyear countdown

---
+++ ForLoop.java
@@ -0,0 +1,12 @@
+public class ForLoop {
+    public static void main(String args[]){
+        //for loop - it can be used to excute a code to certain number of times
+        // for(initialization,condition,update){}
+        // Lets print Countdown to wish happy new year
+        for(int i = 10; i > 0 ; i--){
+            System.out.println(i);
+        }
+        System.out.println("Happy New year !!!!!!");
+    }
+    
+}
