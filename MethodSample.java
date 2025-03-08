8bc9e8cac9240a20c180572fd90a49c1069d17a4
soujanya8 <soujanyagodugu8@gmail.com>
2025-03-07 21:46:59 -0700

    method sample

---
+++ MethodSample.java
@@ -0,0 +1,9 @@
+public class MethodSample {
+    public static void main(String []args){
+        //main method
+        // Method is a Resusable block of code ,can be used by calling() in main method
+
+    }
+
+    
+}
